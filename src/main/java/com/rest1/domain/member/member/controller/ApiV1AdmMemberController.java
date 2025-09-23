08fbb89afc4016d1e019d804399902410676aa63
chacha86 <taejincha0619@gmail.com>
2025-09-23 16:06:27 +0900

    92

--- src/main/java/com/rest1/domain/member/member/controller/ApiV1AdmMemberController.java
+++ src/main/java/com/rest1/domain/member/member/controller/ApiV1AdmMemberController.java
@@ -1,10 +1,7 @@
 package com.rest1.domain.member.member.controller;
 
 import com.rest1.domain.member.member.dto.MemberWithUsernameDto;
-import com.rest1.domain.member.member.entity.Member;
 import com.rest1.domain.member.member.service.MemberService;
-import com.rest1.global.exception.ServiceException;
-import com.rest1.global.rq.Rq;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,18 +17,11 @@ import java.util.List;
 public class ApiV1AdmMemberController {
 
     private final MemberService memberService;
-    private final Rq rq;
 
     @GetMapping
     @Transactional(readOnly = true)
     @Operation(summary = "회원 다건 조회")
     public List<MemberWithUsernameDto> getItems() {
-
-        Member actor = rq.getActor();
-
-        if(!actor.isAdmin()) {
-            throw new ServiceException("403-1", "권한이 없습니다");
-        }
 
         return memberService.findAll().stream()
                 .map(MemberWithUsernameDto::new)
--- src/test/java/com/rest1/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/rest1/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
@@ -72,10 +72,8 @@ public class ApiV1AdmMemberControllerTest {
                 .andDo(print());
 
         resultActions
-                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
-                .andExpect(handler().methodName("getItems"))
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.resultCode").value("403-1"))
-                .andExpect(jsonPath("$.msg").value("권한이 없습니다"));
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
     }
 }
